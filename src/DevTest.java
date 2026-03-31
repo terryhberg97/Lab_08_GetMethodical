6fd4f4a0ac03c6d5493ebf68ced7b4c27d543e11
terryhberg97 <terryhberg97@gmail.com>
2026-03-30 21:16:42 -0400

    Completed Lab 08

--- src/DevTest.java
+++ src/DevTest.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class DevTest {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        String prompt = SafeInput.getNonZeroLenString(in, "Enter ");
+        String prompt = SafeInput.getNonZeroLenString(in, "Enter any string ");
         System.out.println("You entered: " + prompt);
 
         int inputPrompt = SafeInput.getInt(in, "Enter any integer ");
@@ -11,16 +11,16 @@ public class DevTest {
         double inputPrompt2 = SafeInput.getDouble(in, "Enter any double value ");
         System.out.println("The double value you entered is: " + inputPrompt2);
 
-        int inputPrompt3 = SafeInput.getRangedInt(in, "Enter your favorite number ",1,10);
+        int inputPrompt3 = SafeInput.getRangedInt(in, "Enter an inclusive integer ",1,10);
         System.out.println("The number chosen is: " + inputPrompt3);
 
-        double inputPrompt4 = SafeInput.getRangedDouble(in, "Enter your favorite double number", 1, 10);
+        double inputPrompt4 = SafeInput.getRangedDouble(in, "Enter an inclusive double number", 1, 10);
         System.out.println("The double number chosen is: " + inputPrompt4);
 
         boolean inputPrompt5 = SafeInput.getYNConfirm(in, "Enter a yes or no [Y/N] ");
         System.out.println("You chose: " + inputPrompt5);
 
-        String inputPrompt6 = SafeInput.getRegExString(in, "Enter a string that matches the Positive Integers regex pattern ","^\\d{3}-\\d{2}-\\d{4}$");
+        String inputPrompt6 = SafeInput.getRegExString(in, "Enter a string that matches the Positive Integers regex pattern ","^\\d+$");
         System.out.println("Your string matches the pattern: ^\\d+$" );
 
         String firstName = SafeInput.getNonZeroLenString(in, "Enter your first name");
